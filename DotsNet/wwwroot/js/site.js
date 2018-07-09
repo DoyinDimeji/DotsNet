671122047eb2d0395b3876988d4430deea326d28
DoyinDimeji <Oladimeji.Oladoyin@gmail.com>
2018-07-09 10:57:21 +0100

    Basic Layout still

--- DotsNet/wwwroot/js/site.js
+++ DotsNet/wwwroot/js/site.js
@@ -1,1 +1,13 @@
-﻿
+﻿var aboutText1 = 'Hey There'; var aboutText2 = 'What\'s up?'; var aboutText3 = 'Good? OK -_-';
+var aboutElement = document.getElementById("aboutText");
+var aboutAnimation = document.getElementById("aboutText").animate(Infinity);
+var texts = [aboutText1, aboutText2, aboutText3];
+function changeText() {
+    var i = 0;
+    while (true) {
+        while(true) {
+            aboutElement.innerHTML = texts[i];
+            i++;
+        }
+    }
+}
